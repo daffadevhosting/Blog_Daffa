c80f67acb62a7868d6635aeb34f6caf0e1fd01b1
daffadevhosting <ndangsaepudin@outlook.com>
2023-04-07 22:34:44 +0700

    update

---
+++ src/jsfile.js
@@ -0,0 +1,14 @@
+(function(l) {
+    if (l.search[1] === '/' ) {
+      var decoded = l.search.slice(1).split('&').map(function(s) { 
+        return s.replace(/~and~/g, '&')
+      }).join('?');
+      window.history.replaceState(null, null,
+          l.pathname.slice(0, -1) + decoded + l.hash
+      );
+    }
+  }(window.location))
+
+  if (window.location.pathname !== '/' && !window.location.pathname.startsWith('/static')) {
+    window.location.href = '/';
+  }
